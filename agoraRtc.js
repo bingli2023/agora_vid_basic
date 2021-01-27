6e22bb37b0df5c8e794f1348b91f728d7f2046a1
Bing Li <bingli@princeton.edu>
2021-01-26 21:46:03 -0500

    added comments

--- agoraRtc.js
+++ agoraRtc.js
@@ -1,14 +1,21 @@
+// Handle errors.
 let handlefail = function (err) {
   console.log(err);
 };
 
+// Add video streams to the container.
 function addVideoStream(streamId) {
   console.log();
+  // Query the container to which the remote stream belong.
   let remoteContainer = document.getElementById("remoteStream");
+  // Creates a new div for every stream
   let streamDiv = document.createElement("div");
+  // Assigns the elementId to the div.
   streamDiv.id = streamId;
+  // Takes care of the lateral inversion
   streamDiv.style.transform = "rotateY(180deg)";
   streamDiv.style.height = "250px";
+  // Adds the div to the container.
   remoteContainer.appendChild(streamDiv);
 }
 
@@ -17,6 +24,7 @@ document.getElementById("join").onclick = function () {
   let Username = document.getElementById("username").value;
   let appId = "e230b70835a14c70900c7652aec194b7";
 
+  // create a client
   let client = AgoraRTC.createClient({
     mode: "live",
     codec: "h264",
@@ -28,24 +36,31 @@ document.getElementById("join").onclick = function () {
     handlefail
   );
 
+  // join a channel
   client.join(null, channelName, Username, () => {
+    // create a local stream
     var localStream = AgoraRTC.createStream({
       video: true,
       audio: true,
     });
 
+    // initalize the local stream
     localStream.init(function () {
+      // play the local stream
       localStream.play("SelfStream");
       console.log(`App id: ${appId}\nChannel id: ${channelName}`);
+      // publish a local stream
       client.publish(localStream);
     });
   });
 
+  // Subscribe to the remote stream when it is published
   client.on("stream-added", function (evt) {
     console.log("Added Stream");
     client.subscribe(evt.stream, handlefail);
   });
 
+  // Play the remote stream when it is subsribed
   client.on("stream-subscribed", function (evt) {
     console.log("Subscribed Stream");
     let stream = evt.stream;
